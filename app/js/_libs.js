e203a488a94967601341f97b0287811c2c3a1615
karapv <andrewkarapenkov@gmail.com>
2019-09-30 21:54:14 +0300

    Add new fixes

--- app/js/_libs.js
+++ app/js/_libs.js
@@ -119,17 +119,5 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 
 	}
-// Плавный скролл
-    const anchors = document.querySelectorAll('a[href*="#"]')
-    for (let anchor of anchors) {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const blockID = anchor.getAttribute('href').substr(1);
-            document.getElementById(blockID).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            })
-        })
-    }
 });
 
